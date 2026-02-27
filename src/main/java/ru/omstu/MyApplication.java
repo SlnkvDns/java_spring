e7e252468885c8ac82da562a3fc238f0938893e0
Denis Salnikov <denissalnikov8@gmail.com>
2026-02-27 17:40:41 +0600

    add .idea in gitignore

---
+++ src/main/java/ru/omstu/MyApplication.java
@@ -0,0 +1,24 @@
+package ru.omstu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@RestController
+@SpringBootApplication
+public class MyApplication {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+
+}
